3ba13f8c0cf18d7c35852bb1faa44ae6ecf7948e
El-Yacoubi <86795116+elyacoub9@users.noreply.github.com>
2024-12-30 18:41:19 +0100

    bring ntnx-lldp.py here
    the best script ever

---
+++ ntnx-lldp.py
@@ -0,0 +1,42 @@
+import requests
+import urllib3
+import getpass
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+target =input("pe or cvm ip: ")
+username = input("username: ")
+password = getpass.getpass("password: ")
+
+payload = {}
+headers = {'Content-Type': "application/json" }
+auth = (username, password)
+
+#get hosts uuid/ip
+hosts_url = f'https://{target}:9440/PrismGateway/services/rest/v2.0/hosts/'
+hosts_response = requests.get(hosts_url, json=payload, headers=headers, auth=auth,verify=False)
+hosts_data = hosts_response.json()
+entities=hosts_data["entities"]
+hosts={}
+for entity in entities:
+    hosts[entity["uuid"]]=entity["hypervisor_address"]
+    
+#nic to switch port mapp
+
+for uuid, ip in hosts.items():
+    
+    url=f'https://{target}:9440/PrismGateway/services/rest/v2.0/hosts/{uuid}/host_nics'
+    response = requests.get(url, json=payload, headers=headers, auth=auth,verify=False)
+    nics = response.json()
+
+    print(f"===============[ {ip} ]===============")
+
+    for nic in nics:
+        nic_name=nic["name"]
+        TOR_sw_name=nic["switch_device_id"]
+        TOR_sw_port=nic["switch_port_id"]
+        print(f"{nic_name}\t{TOR_sw_name}\t{TOR_sw_port}")
+        
+    
+    
